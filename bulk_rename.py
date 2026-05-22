e215050a1478db0737d7aad4f77ea388c5d7a7e1
SriNotFound <jainsrivats84@gmail.com>
2026-05-22 19:13:37 +0530

    Update bulk_rename.py

--- bulk_rename.py
+++ bulk_rename.py
@@ -1,10 +1,9 @@
 import os
 
-folder  = "C:/Users/PC/Downloads/images/wallpapers"													#enter the path
-file_extention = input(str("What type of file do you want to rename: "))						#enter file extention
-new_name = input(str("What name do you wanna give to your file: "))								#enter new name of 
-																								#that type of files
-a = 36
+folder  = input(str("Enter the path of the folder: "))													#enter the path
+file_extention = input(str("What type of file do you want to rename(example - .txt): "))						#enter file extention
+new_name = input(str("What name do you wanna give to your file(example - cat): "))								#enter new name of that type of files
+a = 1
 
 for filename in os.listdir(folder):
 	if filename.endswith(file_extention):
@@ -12,3 +11,6 @@ for filename in os.listdir(folder):
 		new = os.path.join(folder, f"{new_name}_{a}{file_extention}")
 		os.rename(old ,new)
 		a += 1
+
+#This is safe but always read the code before running it on your device#
+#TOUCH GRASS:)#
